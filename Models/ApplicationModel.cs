875e4c87357084ec7dd6477473af565883ef40e8
VladimirVeligosha <colter228@gmail.com>
2022-05-30 19:25:33 +0300

    Added ApplicationModel

---
+++ Models/ApplicationModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BLL.Models
+{
+    public class ApplicationModel
+    {
+        public string Id { get; set; }
+        public string ApplicantId { get; set; }
+        public string Name { get; set; }
+        //public string College { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+        public bool IsApproved { get; set; } = false;
+
+        public string VacancyId { get; set; }
+        public VacancyModel Vacancy { get; set; }
+    }
+}
