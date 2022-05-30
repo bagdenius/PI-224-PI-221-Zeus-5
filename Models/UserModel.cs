9eab31405ba4f86e206487039a4892a39ac03cd5
Bondarenko Bogdan <5310250@stud.nau.edu.ua>
2022-05-30 19:21:53 +0300

    Added user model

---
+++ Models/UserModel.cs
@@ -0,0 +1,35 @@
+﻿using DAL;
+using DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Models
+{
+    public class UserModel
+    {
+        public Roles Role { get; set; }
+
+        [PersonalData]
+        public string FullName { get; set; }
+
+        [PersonalData]
+        public string ProfilePicture { get; set; }
+
+        [PersonalData]
+        public string Bio { get; set; }
+
+        [PersonalData]
+        public string Sector { get; set; }
+
+        [PersonalData]
+        public string EmployeesCount { get; set; }
+
+        [PersonalData]
+        public string Organisation { get; set; }
+
+        [PersonalData]
+        public bool IsVerified { get; set; }
+
+        [PersonalData]
+        public ResumeModel Resume { get; set; }
+    }
+}
