a782c4eaee05663c1228ce30c1058281de826553
VladimirVeligosha <colter228@gmail.com>
2022-05-30 18:54:06 +0300

    Added Vacany

---
+++ Entities/Vacancy/Application.cs
@@ -0,0 +1,15 @@
+﻿namespace DAL.Entities
+{
+    public class Application
+    {
+        public string Id { get; set; }
+        public string ApplicantId { get; set; }
+        public string Name { get; set; }
+        //public string College { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+        public bool IsApproved { get; set; } = false;
+
+        public string VacancyId { get; set; }
+        public Vacancy Vacancy { get; set; }
+    }
+}
