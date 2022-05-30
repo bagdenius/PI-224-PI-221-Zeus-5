f5fad07cfd7e4acde2110a599a2a08a364011b20
Bondarenko Bogdan <5310250@stud.nau.edu.ua>
2022-05-30 20:30:35 +0300

    Added enum display extension

---
+++ UI/Extensions/EnumDisplayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace UI.Extensions
+{
+    public static class EnumDisplayExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            MemberInfo[] memberInfo = enumValue.GetType().GetMember(enumValue.ToString());
+            Type attributeType = typeof(DisplayAttribute);
+            object[] attributes = memberInfo[0].GetCustomAttributes(attributeType, false);
+            if (attributes == null || attributes.Length != 1)
+                throw new ArgumentOutOfRangeException($"Невозможно найти атрибут по имени '{nameof(DisplayAttribute)}'");
+            var attribute = attributes.SingleOrDefault() as DisplayAttribute;
+            return attribute?.Name;
+        }
+    }
+}
