442a36410943a21cbbfd0e62164ea944cc12b6ab
Bondarenko Bogdan <5310250@stud.nau.edu.ua>
2022-06-01 17:01:20 +0300

    Little fix 4

--- Repositories/ApplicationRepository.cs
+++ Repositories/ApplicationRepository.cs
@@ -23,22 +23,22 @@ namespace Repositories
             select application;
 
         public async Task<Application> GetAsync(string id) =>
-            await _context.Applications/*.AsNoTracking()*/.FirstOrDefaultAsync(v => v.Id == id);
+            await _context.Applications.FirstOrDefaultAsync(v => v.Id == id);
 
         public Application Get(string id) =>
             _context.Applications.AsNoTracking().FirstOrDefault(a => a.Id == id);
 
-        public async Task Add(Application jobApplication) =>
-            await _context.Applications.AddAsync(jobApplication);
+        public async Task Add(Application application) =>
+            await _context.Applications.AddAsync(application);
 
-        public async Task Update(Application jobApplication) =>
-            _context.Applications.Update(jobApplication);
+        public async Task Update(Application application) =>
+            _context.Applications.Update(application);
 
-        public bool CheckApplication(string userid, string jobid)
+        public bool CheckApplication(string userId, string vacancyId)
         {
             var jobApplications = from application in Get()
-                                  where application.Vacancy.Id == jobid
-                                  && application.ApplicantId == userid
+                                  where application.Vacancy.Id == vacancyId
+                                  && application.ApplicantId == userId
                                   select application;
             return jobApplications.Any();
         }
